29adda15b1d0e0df652cf6e0dab065c9ac3ba718
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-12-21 20:29:38 +0200

    feat: add an active page

---
+++ src/pages/active.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+import { Layout, Menu, Main } from "../components";
+
+export const Active: FC = () => {
+  return (
+    <Layout>
+      <>
+        <Menu />
+        <Main />
+      </>
+    </Layout>
+  );
+};
