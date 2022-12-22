cd7591eebb2d297467c51d50dea9057e992ec3c5
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-12-22 22:20:40 +0200

    feat: add the ability to display a modal window also in the process of the template component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,39 @@
-import { FC } from "react";
-import { Layout, Menu, Main } from "../components";
+import { FC, useCallback, useContext } from "react";
+import { Layout, Menu, Main, Button, Modal } from "../components";
+import { showModal } from "../redux/slices/modal-slice";
+import { useAppDispatch, useAppSelector } from "../redux/typed-hooks";
+import { ModalContext } from "../context";
+import { AddTask } from "../components/modal-content";
 
 export const Home: FC = () => {
+  const dispatch = useAppDispatch();
+  const { content, addContentToModal } = useContext(ModalContext)!;
+  const isOpenModal = useAppSelector<boolean>((state) => state.modal.isActive);
+
+  const openModal = useCallback(() => {
+    dispatch(showModal());
+    addContentToModal(<AddTask />);
+  }, []);
+
   return (
     <Layout>
       <>
+        <Modal isOpen={isOpenModal}>{content!}</Modal>
         <Menu />
-        <Main />
+        <Main>
+          <div className="grow">
+            <div className="h-16 border-b-2 border-slate-400">
+              <div className="w-80 h-full flex justify-around items-center">
+                <Button
+                  text="Add a task"
+                  color="btn-success"
+                  onClick={openModal}
+                />
+                <Button text="Delete all" color="btn-error" />
+              </div>
+            </div>
+          </div>
+        </Main>
       </>
     </Layout>
   );
