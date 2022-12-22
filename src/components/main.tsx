e68bd50e1d0c16f9b8a7827fc609308397a2fd77
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-12-22 22:06:00 +0200

    feat: add a component template

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,5 +1,19 @@
 import { FC } from "react";
+import { Button } from "./index";
 
-export const Main: FC = () => {
-  return <div className="grow"></div>;
+interface MainProps {
+  children: JSX.Element;
+}
+
+export const Main: FC<MainProps> = ({ children }) => {
+  return (
+    <div className="flex justify-center items-center grow">
+      <div
+        className="w-full max-w-3xl xl:max-w-5xl h-[700px] flex
+      bg-slate-200 dark:bg-slate-800 rounded-xl border border-slate-400"
+      >
+        {children}
+      </div>
+    </div>
+  );
 };
