3420bad16800de8b6d69443b9de813c9f8403045
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-12-23 18:00:30 +0200

    feat: add the ability to close the modal window by pressing escape and clicking the mouse

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,5 +1,8 @@
-import { FC } from "react";
+import { FC, MouseEvent, KeyboardEvent, useRef, useEffect } from "react";
 import { createPortal } from "react-dom";
+import { ESCAPE } from "../constants";
+import { hideModal } from "../redux/slices/modal-slice";
+import { useAppDispatch } from "../redux/typed-hooks";
 
 const rootPortal: HTMLElement = document.getElementById("portal")!;
 
@@ -9,11 +12,36 @@ interface ModalProps {
 }
 
 export const Modal: FC<ModalProps> = ({ isOpen, children }) => {
+  const dispatch = useAppDispatch();
+  const ref = useRef<HTMLDivElement>(null);
+
+  const closeModal = (): void => {
+    dispatch(hideModal());
+  };
+
+  const closeModalWithEsc = (event: KeyboardEvent<HTMLDivElement>): void => {
+    if (event.code === ESCAPE) closeModal();
+  };
+
+  const closeModalWithMouseClick = (
+    event: MouseEvent<HTMLDivElement>
+  ): void => {
+    if (event.target === event.currentTarget) closeModal();
+  };
+
+  useEffect(() => {
+    if (ref.current) ref.current.focus();
+  });
+
   if (isOpen) {
     return createPortal(
       <div
-        className="absolute inset-0 flex justify-center items-center
-        min-h-screen bg-black/90"
+        ref={ref}
+        tabIndex={0}
+        onKeyDown={closeModalWithEsc}
+        onClick={closeModalWithMouseClick}
+        className="absolute inset-0 flex justify-center 
+        items-center min-h-screen bg-black/90"
       >
         {children}
       </div>,
