2539fd002db4a4f7f6f5b7621c5d9caa15e0c68b
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-12-27 19:16:21 +0200

    feat: add a new slice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,9 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import menuSlice from "./slices/menu-slice";
 import themeSlice from "./slices/theme-slice";
 import modalSlice from "./slices/modal-slice";
+import addTaskSlice from "./slices/add-task-slice";
 
 export const store = configureStore({
-  reducer: { menu: menuSlice, theme: themeSlice, modal: modalSlice },
+  reducer: {
+    menu: menuSlice,
+    theme: themeSlice,
+    modal: modalSlice,
+    addTask: addTaskSlice,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
