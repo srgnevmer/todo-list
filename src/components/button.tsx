d10820f460b3b28dfff67e92c2a7b878f635711f
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-12-27 18:29:01 +0200

    feat: add type

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,23 +1,28 @@
 import { FC, memo } from "react";
 
+type Type = "button" | "submit";
 type Color = "btn-primary" | "btn-success" | "btn-error";
 
 interface ButtonProps {
   text: string;
+  type?: Type;
   color: Color;
   onClick?: () => void;
 }
 
-export const Button: FC<ButtonProps> = memo(({ text, color, onClick }) => {
-  return (
-    <button
-      onClick={onClick}
-      className={`${color}
+export const Button: FC<ButtonProps> = memo(
+  ({ text, type = "button", color, onClick }) => {
+    return (
+      <button
+        type={type}
+        onClick={onClick}
+        className={`${color}
       px-5 py-2 rounded-md
       text-2xl font-semibold text-white 
       transition-colors duration-100 select-none`}
-    >
-      {text}
-    </button>
-  );
-});
+      >
+        {text}
+      </button>
+    );
+  }
+);
