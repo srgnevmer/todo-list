ca4de74db218f14d2a7c8d2e4b4c24869be1a5ca
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-12-28 19:42:46 +0200

    feat: wrap in a memo and rename type

--- src/components/select.tsx
+++ src/components/select.tsx
@@ -1,49 +1,45 @@
-import { FC, useState, useEffect, ChangeEvent } from "react";
-import { getUniqueId, capitalizedFirstLetter } from "../utils";
+import { FC, memo, useState, useEffect, ChangeEvent } from "react";
 import { Priority, Category } from "../types";
+import { getUniqueId, capitalizedFirstLetter } from "../utils";
 
-// TODO: need to change this type
-type Test = "default" | Priority | Category;
+type ValueType = "default" | Priority | Category;
 
 interface SelectProps {
-  value: Test;
+  value: ValueType;
   options: string[];
   placeholder: string;
   onChange: (value: any) => void;
 }
 
-export const Select: FC<SelectProps> = ({
-  value,
-  options,
-  placeholder,
-  onChange,
-}) => {
-  const [selectedValue, setSelectedValue] = useState<Test>(value);
+export const Select: FC<SelectProps> = memo(
+  ({ value, options, placeholder, onChange }) => {
+    const [selectedValue, setSelectedValue] = useState<ValueType>(value);
 
-  const handleChange = (event: ChangeEvent<HTMLSelectElement>): void => {
-    onChange(event.target.value);
-  };
+    const handleChange = (event: ChangeEvent<HTMLSelectElement>): void => {
+      onChange(event.target.value);
+    };
 
-  useEffect(() => {
-    setSelectedValue(value);
-  }, [value]);
+    useEffect(() => {
+      setSelectedValue(value);
+    }, [value]);
 
-  return (
-    <select
-      value={selectedValue}
-      onChange={handleChange}
-      className="w-full max-w-lg text-2xl pl-2 py-1 rounded-lg outline-0
+    return (
+      <select
+        value={selectedValue}
+        onChange={handleChange}
+        className="w-full max-w-lg text-2xl pl-2 py-1 rounded-lg outline-0
       bg-slate-50 dark:bg-slate-600 border-2 border-slate-400
       dark:text-white ring-2 ring-transparent focus:ring-blue-700 cursor-pointer"
-    >
-      <option value="default" hidden>
-        {placeholder}
-      </option>
-      {options.map((option) => (
-        <option key={getUniqueId()} value={option}>
-          {capitalizedFirstLetter(option)}
+      >
+        <option value="default" hidden>
+          {placeholder}
         </option>
-      ))}
-    </select>
-  );
-};
+        {options.map((option) => (
+          <option key={getUniqueId()} value={option}>
+            {capitalizedFirstLetter(option)}
+          </option>
+        ))}
+      </select>
+    );
+  }
+);
