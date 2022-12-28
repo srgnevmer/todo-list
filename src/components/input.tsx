fabc1135f51892e2e4f539610b246f301d45ddfb
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-12-28 19:18:30 +0200

    feat: wrap the component in a memo

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, useEffect, ChangeEvent } from "react";
+import { FC, memo, useState, useEffect, ChangeEvent } from "react";
 
 interface InputProps {
   value: string;
@@ -6,26 +6,28 @@ interface InputProps {
   onChange: (value: string) => void;
 }
 
-export const Input: FC<InputProps> = ({ value, placeholder, onChange }) => {
-  const [text, setText] = useState<string>("");
+export const Input: FC<InputProps> = memo(
+  ({ value, placeholder, onChange }) => {
+    const [text, setText] = useState<string>("");
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
-    onChange(event.target.value);
-  };
+    const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+      onChange(event.target.value);
+    };
 
-  useEffect(() => {
-    setText(value);
-  }, [value]);
+    useEffect(() => {
+      setText(value);
+    }, [value]);
 
-  return (
-    <input
-      type="text"
-      value={text!}
-      onChange={handleChange}
-      placeholder={placeholder}
-      className="w-full max-w-lg text-2xl pl-2 py-1 rounded-lg outline-0
+    return (
+      <input
+        type="text"
+        value={text!}
+        onChange={handleChange}
+        placeholder={placeholder}
+        className="w-full max-w-lg text-2xl pl-2 py-1 rounded-lg outline-0
       bg-slate-50 dark:bg-slate-600 border-2 border-slate-400
       dark:text-white ring-2 ring-transparent focus:ring-blue-700"
-    />
-  );
-};
+      />
+    );
+  }
+);
