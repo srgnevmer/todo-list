6426e5a9bef8cddc0407d45b583882bd1c7eddd0
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-12-31 11:53:04 +0200

    feat: add an animation

--- src/components/alert.tsx
+++ src/components/alert.tsx
@@ -1,5 +1,6 @@
 import { FC, useEffect } from "react";
 import { createPortal } from "react-dom";
+import { animated, config, useTransition } from "@react-spring/web";
 import { AlertType } from "../types";
 import { SvgSelector } from "./index";
 import { useAppDispatch } from "../redux/typed-hooks";
@@ -19,6 +20,13 @@ const rootAlert: HTMLElement = document.getElementById("alert")!;
 export const Alert: FC<AlertProp> = ({ type, message, isOpen }) => {
   const dispatch = useAppDispatch();
 
+  const alertTransition = useTransition(isOpen, {
+    config: config.gentle,
+    from: { opacity: 0, transform: "translateY(-40px)" },
+    enter: { opacity: 1, transform: "translateY(0px)" },
+    leave: { opacity: 0, transform: "translateY(-40px)" },
+  });
+
   useEffect(() => {
     if (isOpen) {
       setTimeout(() => {
@@ -27,23 +35,25 @@ export const Alert: FC<AlertProp> = ({ type, message, isOpen }) => {
     }
   }, [isOpen]);
 
-  if (isOpen) {
-    return createPortal(
-      <div
-        className={`flex absolute top-2 right-2 p-2 rounded-lg
-          ${type === "successe" ? successe : error}`}
-      >
-        <SvgSelector id={type === "successe" ? "check" : "exclamation"} />
-        <span
-          className="text-white text-xl font-semibold
-            flex justify-center items-center"
-        >
-          {message}
-        </span>
-      </div>,
-      rootAlert
-    );
-  }
-
-  return null;
+  return createPortal(
+    alertTransition(
+      (style, item) =>
+        item && (
+          <animated.div
+            style={style}
+            className={`flex absolute top-2 right-2 p-2 rounded-lg
+            ${type === "successe" ? successe : error}`}
+          >
+            <SvgSelector id={type === "successe" ? "check" : "exclamation"} />
+            <span
+              className="text-white text-xl font-semibold
+              flex justify-center items-center"
+            >
+              {message}
+            </span>
+          </animated.div>
+        )
+    ),
+    rootAlert
+  );
 };
