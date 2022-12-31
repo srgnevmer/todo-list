931f8a393f425212832d0a428184e2ab15ecb763
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2022-12-31 11:36:01 +0200

    feat: add an animation

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,5 +1,6 @@
 import { FC, MouseEvent, KeyboardEvent, useRef, useEffect } from "react";
 import { createPortal } from "react-dom";
+import { animated, config, useTransition } from "@react-spring/web";
 import { ESCAPE } from "../constants";
 import { hideModal } from "../redux/slices/modal-slice";
 import { useAppDispatch } from "../redux/typed-hooks";
@@ -14,6 +15,18 @@ interface ModalProps {
 export const Modal: FC<ModalProps> = ({ isOpen, children }) => {
   const dispatch = useAppDispatch();
   const ref = useRef<HTMLDivElement>(null);
+
+  const modalTransition = useTransition(isOpen, {
+    config: config.stiff,
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
+
+  const childTransition = useTransition(true, {
+    from: { transform: "translateY:(-4rem)" },
+    to: { transform: "translateY:(0rem)" },
+  });
 
   const closeModal = (): void => {
     dispatch(hideModal());
@@ -33,21 +46,23 @@ export const Modal: FC<ModalProps> = ({ isOpen, children }) => {
     if (ref.current) ref.current.focus();
   });
 
-  if (isOpen) {
-    return createPortal(
-      <div
-        ref={ref}
-        tabIndex={0}
-        onKeyDown={closeModalWithEsc}
-        onClick={closeModalWithMouseClick}
-        className="absolute inset-0 flex justify-center 
-        items-center min-h-screen bg-black/90"
-      >
-        {children}
-      </div>,
-      rootModal
-    );
-  }
-
-  return null;
+  return createPortal(
+    modalTransition(
+      (style, item) =>
+        item && (
+          <animated.div
+            ref={ref}
+            tabIndex={0}
+            style={style}
+            onKeyDown={closeModalWithEsc}
+            onClick={closeModalWithMouseClick}
+            className="absolute inset-0 flex justify-center 
+            items-center min-h-screen bg-black/90"
+          >
+            {children}
+          </animated.div>
+        )
+    ),
+    rootModal
+  );
 };
