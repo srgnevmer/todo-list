7c644f26c5a753263a3849e453897dfe5c8512b3
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2023-01-03 15:29:10 +0200

    feat: updated the list

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
-export * from "./get-random-int";
+export * from "./format-date";
 export * from "./get-unique-id";
+export * from "./get-random-int";
 export * from "./get-width-window";
 export * from "./capitalized-first-letter";
