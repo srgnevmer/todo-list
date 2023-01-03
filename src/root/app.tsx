b18c7068360c50af37c6441909b7403b69b10c91
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2023-01-03 23:04:15 +0200

    feat: add an application

---
+++ src/root/app.tsx
@@ -0,0 +1,44 @@
+import { FC, useState } from "react";
+import { createStyles } from "@mantine/core";
+import {
+  ColorScheme,
+  MantineProvider,
+  ColorSchemeProvider,
+} from "@mantine/core";
+import { useLocalStorage } from "@mantine/hooks";
+import { Header } from "../ui";
+
+const useStyles = createStyles((theme) => ({
+  container: {
+    minHeight: "100vh",
+    backgroundColor: theme.colorScheme,
+  },
+}));
+
+export const App: FC = () => {
+  const { classes } = useStyles();
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: "mantine-color-scheme",
+    defaultValue: "light",
+  });
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+  return (
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider
+        theme={{ colorScheme }}
+        withNormalizeCSS
+        withGlobalStyles
+      >
+        <div className={classes.container}>
+          <Header />
+        </div>
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
+};
