0795b5a38491c82baae827bbe5c8bfec44dc6773
Sergey Nevmerzhitsky <srgnevmer@gmail.com>
2023-01-06 14:53:16 +0200

    feat: add the ability to display added tasks

--- src/ui/main/styles.ts
+++ src/ui/main/styles.ts
@@ -32,6 +32,8 @@ export const useStyles = createStyles((theme) => ({
   },
 
   main: {
+    display: "flex",
+    flexDirection: "column",
     width: "800px",
     height: "600px",
     border: `1px solid ${
@@ -63,5 +65,92 @@ export const useStyles = createStyles((theme) => ({
     width: "270px",
     display: "flex",
     justifyContent: "space-around",
+  },
+
+  list: {
+    flexGrow: 1,
+    margin: "0px",
+    padding: "10px 5px",
+  },
+
+  task: {
+    width: "100%",
+    height: "56px",
+    display: "flex",
+    listStyle: "none",
+    borderRadius: theme.radius.md,
+    backgroundColor: `${
+      theme.colorScheme === "light"
+        ? theme.colors.gray[2]
+        : theme.colors.dark[6]
+    }`,
+    border: `1px solid ${
+      theme.colorScheme === "light"
+        ? theme.colors.gray[3]
+        : theme.colors.dark[3]
+    }`,
+
+    "&:not(:last-child)": {
+      marginBottom: "10px",
+    },
+  },
+
+  mainSection: {
+    display: "flex",
+    flexGrow: 1,
+    height: "100%",
+  },
+
+  information: {
+    display: "flex",
+    flexGrow: 1,
+    height: "100%",
+  },
+
+  manipulation: {
+    width: "100px",
+    height: "100%",
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+
+  test: {
+    width: "250px",
+    height: "100%",
+    display: "flex",
+  },
+
+  text: {
+    flexGrow: 1,
+    height: "100%",
+    display: "flex",
+    alignItems: "center",
+    paddingLeft: "20px",
+    fontSize: "26px",
+    fontWeight: 600,
+  },
+
+  testOne: {
+    width: "50%",
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    paddingLeft: "5px",
+    fontSize: "15px",
+    fontWeight: 600,
+
+    [`p`]: {
+      margin: "0",
+    },
+  },
+
+  testTwo: {
+    width: "50%",
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   },
 }));
